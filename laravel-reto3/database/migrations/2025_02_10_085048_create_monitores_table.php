05ea61c367272d693578ffec2788f62aa179e73f
inigoo <inigoberastegi@gmail.com>
2025-02-14 10:46:54 +0100

    commit de inigo

--- laravel-reto3/database/migrations/2025_02_10_085048_create_monitores_table.php
+++ laravel-reto3/database/migrations/2025_02_10_085048_create_monitores_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('monitores', function (Blueprint $table) {
             $table->id();
             $table->string('nombre');
+            $table->string('apellido');
             $table->string('foto');
             $table->timestamps();
         });
